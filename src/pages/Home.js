9e6f5c4866b402cd90c92bb619456232e9d82bcc
michelle379 <56913621+michelle379@users.noreply.github.com>
2022-10-02 17:08:55 +0800

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import { Layout } from 'antd';
+import { NavLink } from "react-router-dom";
+import AppHeader from "../components/Header"
+import AppFooter from "../components/Footer"
+import ProductList from "../components/ProductList";
+import products from "../json/products.json";
+
+const { Header, Content, Footer } = Layout;
+
+
+
+function Home() {
+  return (
+    <Layout className="container main-layout">
+      <Header className="layout-header">
+        <AppHeader title="NORDIC NEST Shopping Cart"/>
+      </Header>
+      <Content className="layout-content">
+        <ProductList products={products}/>
+      </Content>
+      <Footer className="layout-footer">
+        <AppFooter/>  
+      </Footer>      
+    </Layout>
+  );
+}
+
+export default Home;
