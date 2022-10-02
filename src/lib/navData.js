c695fb107793d396548a7bf2b61aca567925882d
michelle379 <56913621+michelle379@users.noreply.github.com>
2022-10-02 17:08:06 +0800

    Add files via upload

---
+++ src/lib/navData.js
@@ -0,0 +1,52 @@
+import HomeIcon from '@mui/icons-material/Home';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import ChairIcon from '@mui/icons-material/Chair';
+import WbIncandescentIcon from '@mui/icons-material/WbIncandescent';
+import SoupKitchenIcon from '@mui/icons-material/SoupKitchen';
+import CasinoIcon from '@mui/icons-material/Casino';
+import SportsMmaIcon from '@mui/icons-material/SportsMma';
+ 
+export const navData = [
+    {
+        id: 0,
+        icon: <HomeIcon/>,
+        text: "Home",
+        link: "/"
+    },
+    {
+        id: 1,
+        icon: <RestaurantIcon/>,
+        text: "Tableware",
+        link: "/tableware"
+    },
+    {
+        id: 2,
+        icon: <SoupKitchenIcon/>,
+        text: "Cookware",
+        link: "/cookware"
+    },
+    {
+        id: 3,
+        icon: <CasinoIcon/>,
+        text: "Home-accessories",
+        link: "/home-accessories"
+    },
+    {
+        id: 4,
+        icon: <WbIncandescentIcon />,
+        text: "Lighting",
+        link: "/lighting"
+    },
+    {
+        id: 5,
+        icon: <SportsMmaIcon/>,
+        text: "Textile",
+        link: "/textile"
+    },
+    {
+        id: 6,
+        icon: <ChairIcon/>,
+        text: "Furniture",
+        link: "/furniture"
+    }
+]
